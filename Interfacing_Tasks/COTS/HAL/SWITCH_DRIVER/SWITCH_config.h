5ae0ef76245add9bc1450fd66d5fed7146bff839
Mohammad-Tamerr <mohammadtamer1212@gmail.com>
2024-03-09 01:43:52 +0200

    Embedded

---
+++ Interfacing_Tasks/COTS/HAL/SWITCH_DRIVER/SWITCH_config.h
@@ -0,0 +1,13 @@
+/*
+ * SWITCH_config.h
+ *
+ *  Created on: Mar 9, 2024
+ *      Author: dell
+ */
+
+#ifndef HAL_SWITCH_DRIVER_SWITCH_CONFIG_H_
+#define HAL_SWITCH_DRIVER_SWITCH_CONFIG_H_
+
+
+
+#endif /* HAL_SWITCH_DRIVER_SWITCH_CONFIG_H_ */
---
+++ Interfacing_Tasks/COTS/HAL/SWITCH_DRIVER/SWITCH_private.h
@@ -0,0 +1,13 @@
+/*
+ * SWITCH_private.h
+ *
+ *  Created on: Mar 9, 2024
+ *      Author: dell
+ */
+
+#ifndef HAL_SWITCH_DRIVER_SWITCH_PRIVATE_H_
+#define HAL_SWITCH_DRIVER_SWITCH_PRIVATE_H_
+
+
+
+#endif /* HAL_SWITCH_DRIVER_SWITCH_PRIVATE_H_ */
